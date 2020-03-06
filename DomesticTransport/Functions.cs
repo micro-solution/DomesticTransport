d46d67287877636bca0a1d520fcf3413c412c060
Алексей Евсеев <aevseev@micro-solution.ru>
2020-03-06 12:32:39 +0300

    Complete Aouto

--- DomesticTransport/Functions.cs
+++ DomesticTransport/Functions.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using Config = DomesticTransport.Properties.Settings;
@@ -49,13 +50,13 @@ namespace DomesticTransport
 
                 ClearListObj(carrierTable);
                 if (invoiciesTable.DataBodyRange.Rows.Count > 0)
-                { invoiciesTable.DataBodyRange.Rows.Delete();}
+                { invoiciesTable.DataBodyRange.Rows.Delete(); }
 
                 if (deliveries != null)
                 {
                     foreach (Delivery delivery in deliveries)
                     {
-                        PrintDelivery(delivery, deliverySheet,carrierTable, invoiciesTable);
+                        PrintDelivery(delivery, deliverySheet, carrierTable, invoiciesTable);
                         System.Windows.Forms.Application.DoEvents();
                     }
 
@@ -65,34 +66,31 @@ namespace DomesticTransport
 
         private void ClearListObj(ListObject listObject)
         {
-          
+
             Worksheet worksheet = listObject.Parent;
-            for (int i= listObject.ListRows.Count; i>0;i--)
+            for (int i = listObject.ListRows.Count; i > 0; i--)
             {
-                    ListRow listRow = listObject.ListRows[i];
+                ListRow listRow = listObject.ListRows[i];
                 worksheet.Rows[listRow.Range.Row].Delete();
             }
-            
+
         }
         private void AddListRow(ListObject listObject)
         {
             Worksheet worksheet = listObject.Parent;
-            if ( listObject.ListRows.Count > 0)
+            if (listObject.ListRows.Count > 0)
             {
-            worksheet.Rows[listObject.ListRows[listObject.ListRows.Count].Range.Row + 1].Insert();
+                worksheet.Rows[listObject.ListRows[listObject.ListRows.Count].Range.Row + 1].Insert();
             }
             else
             {
-                worksheet.Rows[listObject.HeaderRowRange.Row+2].Insert();
+                worksheet.Rows[listObject.HeaderRowRange.Row + 2].Insert();
             }
             listObject.ListRows.Add();
         }
 
         private void PrintDelivery(Delivery delivery, Worksheet deliverySheet, ListObject CarrierTable, ListObject OrderTable)
-        {
-            //Worksheet deliverySheet = Globals.ThisWorkbook.Sheets["Delivery"];
-            //ListObject CarrierTable = deliverySheet.ListObjects["TableCarrier"];
-            //ListObject InvoiciesTable = deliverySheet.ListObjects["TableInvoicies"];
+        {            
             if (CarrierTable == null || OrderTable == null)
             {
                 MessageBox.Show("Отсутствует таблица");
@@ -107,56 +105,44 @@ namespace DomesticTransport
             else
             {
                 AddListRow(CarrierTable);
-                rowCarrier = CarrierTable.ListRows[CarrierTable.ListRows.Count-1];
-            }                
-                rowCarrier.Range[1, 1].Value = rowCarrier.Index;
-                
-                //  rowCarrier.Range[1, 1].Value = delivery.Carrier?.Id  ?? 0 ;
-                // rowCarrier.Range[1, 2].Value = delivery.Carrier?.Name ?? "";
-                // rowCarrier.Range[1, 3].Value = delivery.Carrier?.Truck?.Number ?? "";
-                //  rowCarrier.Range[1, 4].Value = delivery.Carrier?.Truck?.Mark ?? "";
-                rowCarrier.Range[1, 5].Value = delivery.Carrier?.Truck?.Tonnage ?? 0;
-                rowCarrier.Range[1, 6].Value = delivery.Carrier?.Name ?? "";
+                rowCarrier = CarrierTable.ListRows[CarrierTable.ListRows.Count - 1];
+            }
+            rowCarrier.Range[1, 1].Value = rowCarrier.Index;
 
-                ListRow rowOrder;
+            //  rowCarrier.Range[1, 1].Value = delivery.Carrier?.Id  ?? 0 ;
+            // rowCarrier.Range[1, 2].Value = delivery.Carrier?.Name ?? "";
+            // rowCarrier.Range[1, 3].Value = delivery.Carrier?.Truck?.Number ?? "";
+            //  rowCarrier.Range[1, 4].Value = delivery.Carrier?.Truck?.Mark ?? "";
+            rowCarrier.Range[1, 5].Value = delivery.Carrier?.Truck?.Tonnage ?? 0;
+            rowCarrier.Range[1, 6].Value = delivery.Carrier?.Name ?? "";
 
-                foreach (Order order in delivery.Invoices)
+            ListRow rowOrder;
+
+            foreach (Order order in delivery.Invoices)
+            {
+                // if (CarrierTable.ListRows.Count == 0) CarrierTable.ListRows.AddEx()
+
+                if (OrderTable.ListRows.Count == 0)
                 {
-                    // if (CarrierTable.ListRows.Count == 0) CarrierTable.ListRows.AddEx()
-
-                    if (OrderTable.ListRows.Count == 0)
-                    {
-                        OrderTable.ListRows.Add();
+                    OrderTable.ListRows.Add();
                     rowOrder = OrderTable.ListRows[1];
-                    }
-                    else
-                    {
-                        OrderTable.ListRows.Add();
-                        rowOrder = OrderTable.ListRows[OrderTable.ListRows.Count-1]; // InvoiciesTable.ListRows.AddEx(InvoiciesTable.ListRows.Count - 1);
-                    }
-                    int column = 0;
-                        rowOrder.Range[1, ++column].Value = rowCarrier.Index; //delivery.Carrier?.Id ?? 0;
-                        rowOrder.Range[1, ++column].Value = order.TransportationUnit;
-                        rowOrder.Range[1, ++column].Value = order.Customer?.Id ?? 0;
-                        rowOrder.Range[1, ++column].Value = "";
-                        rowOrder.Range[1, ++column].Value = order.Route;
-                        rowOrder.Range[1, ++column].Value = order.PalletsCount;
-                        rowOrder.Range[1, ++column].Value = order.Weight;
-                        rowOrder.Range[1, ++column].Value = order.Cost;
-
                 }
+                else
+                {
+                    OrderTable.ListRows.Add();
+                    rowOrder = OrderTable.ListRows[OrderTable.ListRows.Count - 1]; 
+                }
+                int column = 0;
+                rowOrder.Range[1, ++column].Value = rowCarrier.Index;
+                rowOrder.Range[1, ++column].Value = order.TransportationUnit;
+                rowOrder.Range[1, ++column].Value = order.Customer?.Id ?? "";
+                rowOrder.Range[1, ++column].Value = "";
+                rowOrder.Range[1, ++column].Value = order.Route;
+                rowOrder.Range[1, ++column].Value = order.PalletsCount;
+                rowOrder.Range[1, ++column].Value = order.Weight;
+                rowOrder.Range[1, ++column].Value = order.Cost;
 
-
-                //int offset = number > 1 ? 20 : Config.Default.HeaderRow;
-                //int tableRows = 10;
-                //int tableColumns = 10;
-
-                // Range range = deliverySheet.Range[deliverySheet.Cells[number + offset, 2],
-                //                                         deliverySheet.Cells[number + offset + tableRows, 2 + tableColumns]];
-                //range.Cells[1, 1].Value = "s11s";
-                // range.Cells[2, 1].Value = delivery.DateCreate.ToString(); 
-            
-
+            }
         }
 
         /// <summary>
@@ -165,8 +151,8 @@ namespace DomesticTransport
         /// <param name="sap"></param>
         /// <returns></returns>
         public List<Delivery> GetDeliveries(string sap, string orders)
-        {
-           List<Delivery> deliveries = null;
+        {           
+            List<Order> rourerOrders=new List<Order> ();
             Delivery delivery = null;
             Workbook sapBook = null;
             Workbook orderBook = null;
@@ -184,53 +170,72 @@ namespace DomesticTransport
             {
                 int lastRow = sheet.Cells[sheet.Rows.Count, 1].End(XlDirection.xlUp).Row;
                 int lastColumn = sheet.UsedRange.Column + sheet.UsedRange.Columns.Count - 1;
-                Range range = sheet.Range[sheet.Cells[2, 1], sheet.Cells[lastRow, lastColumn]];         
+                Range range = sheet.Range[sheet.Cells[2, 1], sheet.Cells[lastRow, lastColumn]];
 
                 foreach (Range row in range.Rows)
                 {
                     Order order = ReadSapRow(row);
                     if (order != null)
                     {
-                        Debug.WriteLine(row.Row+ " "+ order.TransportationUnit );
+
                         if (!string.IsNullOrWhiteSpace(order.TransportationUnit))
                         {
-                            List<string>  orderInfo = GetOrderInfo(orderBook.Sheets[1], order.TransportationUnit);
+                            List<string> orderInfo = GetOrderInfo(orderBook.Sheets[1], order.TransportationUnit);
                             if (orderInfo != null)
                             {
                                 string costStr = orderInfo[1];
-                                costStr= costStr.Replace("Стоимость товаров без НДС:","");
+                                costStr = costStr.Replace("Стоимость товаров без НДС:", "");
                                 costStr = costStr.Replace("RUB", "");
-                                costStr = costStr.Replace(".", "");                                
+                                costStr = costStr.Replace(".", "");
                                 costStr = costStr.Trim();
                                 order.Cost = double.TryParse(costStr, out double cost) ? cost : 0;
+                                string pallets = orderInfo.Find(x => x.Contains("грузовых мест:")) ?? "";
+                                pallets = string.Join("", pallets.Where(c => char.IsDigit(c)));               
+                                order.PalletsCount = int.TryParse(pallets, out int p ) ? p : 0;
 
-                               // order.Customer.   //Улица , Город
+                                // order.Customer.   //Улица , Город
                             }
                         }
-
-
-
-                        delivery = deliveries?.Find(d => d.Invoices.Find(i => i.Route == order.Route) != null);
-                        if (delivery != null)
-                        {
-                            delivery.Invoices.Add(order);
-                        }
-                        else
-                        {
-                            delivery = new Delivery();
-                            delivery.Invoices = new List<Order>();
-                            delivery.Invoices.Add(order);
-                            if (deliveries == null) deliveries = new List<Delivery>();
-                            deliveries.Add(delivery);
-                        }
+                        rourerOrders.Add(order);
                     }
+                } 
+            }
+            return CompleteAuto(rourerOrders); 
+        }
 
-                    //Комплектовать авто
+        /// <summary>
+        /// Распределить заказы по автомобилям
+        /// </summary>
+        /// <param name="orders"></param>
+        /// <returns></returns>
+        public List<Delivery> CompleteAuto(List<Order> orders)
+        {
+            List<Delivery> deliveries = new List<Delivery>();            
+            List<string> routes = new List<string>();           
+            orders.GroupBy(r => r.Route).ToList().Distinct();
+            
+            //Уникальные маршруты
+            routes = (from r in orders
+                      select r.Route).Distinct().ToList();            
+            foreach(string route in routes)
+            {
 
-
-                }
 
             }
+
+
+            //delivery = deliveries?.Find(d => d.Invoices.Find(i => i.Route == order.Route) != null);
+            //if (delivery != null)
+            //{
+            //    delivery.Invoices.Add(order);
+            //}
+            //else
+            //{
+            //    delivery = new Delivery();
+            //    delivery.Invoices = new List<Order>();
+            //    delivery.Invoices.Add(order);
+            //    if (deliveries == null) deliveries = new List<Delivery>();
+            //    deliveries.Add(delivery);
             return deliveries;
         }
 
@@ -244,38 +249,41 @@ namespace DomesticTransport
             /// ТТН
             Order order = new Order();
             Debug.WriteLine(row.Row);
-          order.TransportationUnit = row.Cells[1, 4].Text ;
-          string idCusomer = row.Cells[1, 5].Text;
-          order.Id = row.Cells[1, 7].Text;
+
+            order.TransportationUnit = row.Cells[1, GetColumn(row.Parent, "Transportation Unit", 1)].Text;
+            if (string.IsNullOrWhiteSpace(order.TransportationUnit)) return null;
+
+
+            string idCusomer = row.Cells[1, GetColumn(row.Parent, "Получатель материала", 1)].Text;
+            order.Customer = string.IsNullOrWhiteSpace(idCusomer) ? null : new Customer(idCusomer);
+
+            order.Id = row.Cells[1, GetColumn(row.Parent, "Delivery", 1)].Text;
+
             if (string.IsNullOrWhiteSpace(idCusomer) || string.IsNullOrWhiteSpace(order.Id))
             {
                 return null;
-            }            
-                order.Customer = new Customer(idCusomer);                       
-
-            string weight = row.Cells[1, 8].Text;
+            }
+            string weight = row.Cells[1, GetColumn(row.Parent, "Вес брутто", 1)].Text;
             order.Weight = double.TryParse(weight, out double wgt) ? wgt : 0;
-
-            string palletsCount = row.Cells[1, 9].Text;
-            order.PalletsCount = int.TryParse(palletsCount, out int count) ? count : 0;
-           
-            order.Route = row.Cells[1, 11].Text;
-
-
-                //order.Customer = string.IsNullOrWhiteSpace(idCusomer) ? null : new Customer(idCusomer);
-            //string idDocInvoice = row.Cells[1, 3].Value;
-          //  if (string.IsNullOrWhiteSpace(idDocInvoice)) return null;
-          //  order.Id = int.TryParse(idDocInvoice, out int id) ? id : 0;
+            order.Route = row.Cells[1, GetColumn(row.Parent, "Маршрут", 1)].Text;
             return order;
         }
 
-        private List<string>  GetOrderInfo(Worksheet sheet, string transportationUnit)
+
+
+        /// <summary>
+        /// Записать накладную по доставке из файла выгруза в Лист
+        /// </summary>
+        /// <param name="sheet"></param>
+        /// <param name="transportationUnit"></param>
+        /// <returns></returns>
+        private List<string> GetOrderInfo(Worksheet sheet, string transportationUnit)
         {
             Range findRange = sheet.Columns[1];
             //string search = "№ ТТН:" + new string('0', 18 - transportationUnit.Length) + transportationUnit;
             string search = new string('0', 18 - transportationUnit.Length) + transportationUnit;
             Range fcell = findRange.Find(What: search, LookIn: XlFindLookIn.xlValues);
-          
+
             if (fcell == null && fcell.Value.Trim().Contains("ТТН:")) return null;
 
             int rowStart = fcell.Row;
@@ -288,15 +296,14 @@ namespace DomesticTransport
                 fcell = findRange.Cells[rowEnd++, 1];
                 string cellText = fcell.Value;
                 cellText.Trim();
-                cellText = cellText.Replace("\t","");
+                cellText = cellText.Replace("\t", "");
                 cellText = cellText.Replace(";;;", "");
                 if (string.IsNullOrEmpty(cellText.Replace(";", ""))) break;
-                info.Add(cellText);                
+                info.Add(cellText);
             }
             while (rowEnd <= lastRow);
             return info; //findRange[findRange.Cells[rowStart, 1], findRange.Cells[rowEnd, 1]];
         }
-
 
         /// <summary>
         /// Ищет в строке или на листе ячейку с заголовком и возвращает столбец
--- DomesticTransport/Model/Customer.cs
+++ DomesticTransport/Model/Customer.cs
@@ -2,7 +2,7 @@
 {
     public class Customer
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Addres { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
@@ -11,7 +11,7 @@
         Customer() { }
      public   Customer(string id)
         {
-        
+            Id = id;
         }
     }
 }
